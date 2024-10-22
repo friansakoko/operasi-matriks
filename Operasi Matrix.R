05f7df74e4b8c594876a2b684545d05d0fc9feb1
Koko Friansa <koko.friansa@tse.itera.ac.id>
2024-10-22 17:45:00 +0700

    Add file via upload
    New document

---
+++ Operasi Matrix.R
@@ -0,0 +1,30 @@
+# Create two 2x3 matrices.
+matrix1 <- matrix(c(1, 2, 3, 4, 5, 6), nrow = 3)
+matrix2 <- matrix(c(7, 8, 9, 10, 11, 12), nrow = 3)
+
+# Add the matrices
+result1 <- matrix1 + matrix2
+
+# Subtract the matrices
+result2 <- matrix1 - matrix2
+
+# Multiply the matrices
+result3 <- matrix1 * matrix2
+
+# Divide the matrices
+result4 <- matrix1 / matrix2
+
+# Print
+print("The first matrix is:")
+print(matrix1)
+print("The second matrix is:")
+print(matrix2)
+# Print results
+print("The result of Addition of is:")
+print(result1)
+print("The result of Subtraction of is:")
+print(result2)
+print("The result of Multiplication of is:")
+print(result3)
+print("The result of Division of is:")
+print(result4)
